b26bc0556d1a228a2bc53d60b725bc258c79c177
Ramsingh20 <ram02june2003@gmail.com>
2026-02-15 16:59:03 +0530

    Admin Projects (Services) Added

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -16,12 +16,25 @@ const getAllUsers = async (req, res, next) => {
     }
 };
 
+// Naya Project Add karne ke liye
 const addService = async (req, res) => {
   try {
-    const response = req.body;
-    await Service.create(response);
-    return res.status(200).json({ message: "Project added successfully" });
+    const { service, description, price, provider } = req.body;
+    
+    // Naya data create karna
+    const serviceCreated = await Service.create({
+      service,
+      description,
+      price, // Isko hum Tech Stack ki tarah use kar rahe hain
+      provider, // Isko hum GitHub link ki tarah use kar rahe hain
+    });
+
+    return res.status(200).json({ 
+      message: "Project added successfully!", 
+      data: serviceCreated 
+    });
   } catch (error) {
+    console.log("Error adding service:", error);
     return res.status(500).json({ message: "Project not added" });
   }
 };
